dd93c651bb65930f58e9b067a0bcf49b8017731b
a00837024 <a00837024@tec.mx>
2024-10-07 10:59:59 -0600

    .

--- app/src/main/java/com/example/binarybrainz/Clientes/VistaServicios.kt
+++ app/src/main/java/com/example/binarybrainz/Clientes/VistaServicios.kt
@@ -1,5 +1,5 @@
 package com.example.binarybrainz.Clientes
-
+// a
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState

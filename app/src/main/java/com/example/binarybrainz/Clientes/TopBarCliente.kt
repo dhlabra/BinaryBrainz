ce6b67e631e2a91823bde695661df581ac4d799a
ricardochapa203 <a01722755@tec.mx>
2024-10-15 21:37:51 -0600

    TopBarCliente color DarkGrey

--- app/src/main/java/com/example/binarybrainz/Clientes/TopBarCliente.kt
+++ app/src/main/java/com/example/binarybrainz/Clientes/TopBarCliente.kt
@@ -1,16 +1,15 @@
 package com.example.binarybrainz.Clientes
 
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.binarybrainz.Extras.UserViewModel
+import com.example.binarybrainz.ui.theme.DarkGrey // Importa el color DarkGrey
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -21,20 +20,26 @@ fun TopBarClientes(navController: NavController, clientName: String, viewModel: 
                 text = clientName,
                 fontSize = 20.sp,
                 modifier = Modifier.fillMaxWidth(),
-                style = MaterialTheme.typography.titleMedium
+                style = MaterialTheme.typography.titleMedium,
+                color = MaterialTheme.colorScheme.onPrimary // Aplica el color adecuado para el texto
             )
         },
         actions = {
             IconButton(
                 onClick = {
-                    viewModel.signOut()
+                    viewModel.signOut() // Acción para cerrar sesión
                 }
             ) {
-                Icon(Icons.Default.Person, contentDescription = "Perfil de Cliente")
+                Icon(
+                    Icons.Default.Person,
+                    contentDescription = "Perfil de Cliente",
+                    tint = MaterialTheme.colorScheme.onPrimary // Aplica el color adecuado para el ícono
+                )
             }
         },
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp)
+        colors = TopAppBarDefaults.topAppBarColors(
+            containerColor = DarkGrey // Aplica el color DarkGrey al fondo
+        ),
+        modifier = Modifier.fillMaxWidth() // Elimina el padding para ocupar todo el ancho
     )
 }
